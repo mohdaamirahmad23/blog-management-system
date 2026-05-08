d61add9fad877501147fb89c806eb13de498be75
mohdaamirahmad23 <aamirahamed140@gmail.com>
2026-05-09 00:45:16 +0530

    fixed routes

--- routes/web.php
+++ routes/web.php
@@ -9,18 +9,16 @@ Route::get('/', function () {
 
 Route::get('/admin/login', function () {
 
-    return view('login');
+    if(session()->has('admin'))
+    {
+        return redirect('/dashboard');
+    }
 
+    return view('login');
 });
 
 Route::post('/admin/login', [PostController::class, 'login']);
-Route::post('/add-post', [PostController::class, 'store']);
-Route::get('/delete-post/{id}', [PostController::class, 'delete']);
-Route::get('/edit-post/{id}', [PostController::class, 'edit']);
 
-Route::post('/update-post/{id}', [PostController::class, 'update']);
-Route::get('/filter-posts', [PostController::class, 'filter']);
-Route::get('/blog/{id}', [PostController::class, 'show']);
 Route::get('/dashboard', function () {
 
     if(!session()->has('admin'))
@@ -29,8 +27,14 @@ Route::get('/dashboard', function () {
     }
 
     return app()->call([App\Http\Controllers\PostController::class, 'index']);
-
 });
+
+Route::post('/add-post', [PostController::class, 'store']);
+Route::get('/delete-post/{id}', [PostController::class, 'delete']);
+Route::get('/edit-post/{id}', [PostController::class, 'edit']);
+Route::post('/update-post/{id}', [PostController::class, 'update']);
+Route::get('/filter-posts', [PostController::class, 'filter']);
+Route::get('/blog/{id}', [PostController::class, 'show']);
 Route::get('/filter-date', [PostController::class, 'filterDate']);
 Route::get('/search-posts', [PostController::class, 'search']);
 Route::get('/logout', [PostController::class, 'logout']);

dfd8cf13ef05a090af09a40e0773e1e348f52b15
mohdaamirahmad23 <aamirahamed140@gmail.com>
2026-05-09 00:36:27 +0530

    fixed dashboard session redirect

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::get('/', function(){
-
-    if(!session()->has('admin'))
-    {
-        return redirect('/admin/login');
-    }
-
+Route::get('/', function () {
     return redirect('/dashboard');
-
 });
 
 Route::get('/admin/login', function () {
@@ -28,7 +21,16 @@ Route::get('/edit-post/{id}', [PostController::class, 'edit']);
 Route::post('/update-post/{id}', [PostController::class, 'update']);
 Route::get('/filter-posts', [PostController::class, 'filter']);
 Route::get('/blog/{id}', [PostController::class, 'show']);
-Route::get('/dashboard', [PostController::class, 'index']);
+Route::get('/dashboard', function () {
+
+    if(!session()->has('admin'))
+    {
+        return redirect('/admin/login');
+    }
+
+    return app()->call([App\Http\Controllers\PostController::class, 'index']);
+
+});
 Route::get('/filter-date', [PostController::class, 'filterDate']);
 Route::get('/search-posts', [PostController::class, 'search']);
 Route::get('/logout', [PostController::class, 'logout']);
